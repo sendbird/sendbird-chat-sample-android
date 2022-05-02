6e7aec13b361aaa138010ffcef1ce404bb9cf5c3
mihaimoiseanu <mihai.moiseanu@gmail.com>
2022-05-02 04:31:50 +0300

    48-user-android-profile-image

---
+++ openchannel-profile-image/src/main/java/com/sendbird/chat/sample/openchannel/profileimage/ui/main/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.sendbird.chat.sample.openchannel.profileimage.ui.main
+
+import androidx.fragment.app.Fragment
+import com.sendbird.chat.module.ui.base.BaseMainActivity
+import com.sendbird.chat.sample.openchannel.profileimage.ui.openchannel.OpenChannelListFragment
+
+class MainActivity : BaseMainActivity() {
+    override fun getFragmentItems(): List<Fragment> = listOf(OpenChannelListFragment())
+}
