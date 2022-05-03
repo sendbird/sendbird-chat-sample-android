abd6ea52b00d0424ed790eb7209a5aebcc28a50f
Mihai Moiseanu <mihai.moiseanu@gmail.com>
2022-05-03 06:08:36 +0300

    duplicate sample, get translated message from push notification

---
+++ groupchannel-push-translations/src/main/java/com/sendbird/chat/sample/groupchannel/pushtranslations/ui/Notifications.kt
@@ -0,0 +1,56 @@
+package com.sendbird.chat.sample.groupchannel.pushtranslations.ui
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.sendbird.chat.sample.groupchannel.pushtranslations.R
+
+
+private const val ChannelId = "channel_id"
+private const val NotificationId = 1
+
+fun sendNotification(
+    context: Context,
+    messageTitle: String,
+    messageBody: String,
+    channelUrl: String
+) {
+
+    val notification = NotificationCompat.Builder(context, channelUrl)
+        .setSmallIcon(R.drawable.ic_logo_symbol)
+        .setColor(Color.parseColor("#7469C4")) // small icon background color
+        .setLargeIcon(BitmapFactory.decodeResource(context.resources, R.drawable.ic_logo_horizontal))
+        .setContentTitle(messageTitle)
+        .setContentText(messageBody)
+        .setAutoCancel(true)
+        .build()
+
+    context.createNotificationChannel()
+
+    with(NotificationManagerCompat.from(context)) {
+        notify(NotificationId, notification)
+    }
+}
+
+
+private fun Context.createNotificationChannel() {
+    // Create the NotificationChannel, but only on API 26+ because
+    // the NotificationChannel class is new and not in the support library
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val name = getString(R.string.channel_name)
+        val descriptionText = getString(R.string.channel_description)
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(ChannelId, name, importance).apply {
+            description = descriptionText
+        }
+        // Register the channel with the system
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+}
