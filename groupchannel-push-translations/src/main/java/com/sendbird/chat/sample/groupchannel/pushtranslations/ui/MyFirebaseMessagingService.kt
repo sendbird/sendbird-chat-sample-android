c8256a2fd283195b2dcbb2d2c6d907709bba9798
mihaimoiseanu <mihai.moiseanu@gmail.com>
2022-05-04 20:22:00 +0300

    duplicate sample, get translated message from push notification (#96)

---
+++ groupchannel-push-translations/src/main/java/com/sendbird/chat/sample/groupchannel/pushtranslations/ui/MyFirebaseMessagingService.kt
@@ -0,0 +1,52 @@
+package com.sendbird.chat.sample.groupchannel.pushtranslations.ui
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.sendbird.android.SendbirdChat
+import org.json.JSONObject
+import java.util.*
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        SendbirdChat.registerPushToken(token) { status, e ->
+            if (e != null) {
+                e.printStackTrace()
+                return@registerPushToken
+            }
+
+            if (status == SendbirdChat.PushTokenRegistrationStatus.PENDING) {
+                // A token registration is pending.
+                // Retry the registration after a connection has been successfully established.
+            }
+        }
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        try {
+            if (remoteMessage.data.containsKey("sendbird")) {
+                val sendbird = remoteMessage.data["sendbird"]?.let { JSONObject(it) } ?: return
+                val channel = sendbird.get("channel") as JSONObject
+                val channelUrl = channel["channel_url"] as String
+                val messageTitle = sendbird.get("push_title") as String
+                val messageBody = getTranslatedMessage(sendbird)
+                // If you want to customize a notification with the received FCM message,
+                // write your method like sendNotification() below.
+                sendNotification(applicationContext, messageTitle, messageBody, channelUrl)
+            }
+        } catch (exception: Exception) {
+
+        }
+    }
+
+    private fun getTranslatedMessage(jsonObject: JSONObject): String {
+        val language = Locale.getDefault().language
+        val translations = jsonObject.getJSONObject("translations")
+        return try {
+            translations.getString(language)
+        } catch (exception: Exception) {
+            jsonObject.getString("message")
+        }
+    }
+}
